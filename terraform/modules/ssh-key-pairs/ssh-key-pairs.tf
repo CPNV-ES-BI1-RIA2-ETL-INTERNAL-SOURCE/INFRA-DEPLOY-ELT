55ea9a194ca8cbe45c6b92cf01b488b7e8904a8d
YFanha <yannfnha@gmail.com>
2025-03-19 15:43:43 +0100

    feat(terraform): exporting private key for clients

--- terraform/modules/ssh-key-pairs/ssh-key-pairs.tf
+++ terraform/modules/ssh-key-pairs/ssh-key-pairs.tf
@@ -8,3 +8,9 @@ resource "aws_key_pair" "clients_sshkey" {
     key_name   = var.sshkey_list[count.index]
     public_key = tls_private_key.ssh_key_pairs[count.index].public_key_openssh
 }
+
+resource "local_file" "ssh_private_key" {
+  count    = length(var.sshkey_list)
+  filename = "${path.module}/../../${var.sshkey_list[count.index]}"
+  content  = tls_private_key.ssh_key_pairs[count.index].private_key_openssh
+}
