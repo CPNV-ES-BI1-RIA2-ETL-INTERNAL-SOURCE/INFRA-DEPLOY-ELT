eae90d5abdbb834adcf8c54921d31c8e69111401
YFanha <yannfnha@gmail.com>
2025-03-19 19:45:23 +0100

    feat(terraform): export ssh key as clients

--- terraform/modules/ssh-key-pairs/ssh-key-pairs.tf
+++ terraform/modules/ssh-key-pairs/ssh-key-pairs.tf
@@ -14,3 +14,15 @@ resource "local_file" "ssh_private_key" {
   filename = "${path.module}/../../${var.sshkey_list[count.index]}"
   content  = tls_private_key.ssh_key_pairs[count.index].private_key_openssh
 }
+
+resource "local_file" "clients" {
+  filename = "${path.module}/../../../ansible/vars/clients.yml"
+  content = yamlencode({
+    clients   = [
+      for idx, client_name in var.sshkey_list : {
+        name = client_name
+        ssh_key = tls_private_key.ssh_key_pairs[idx].public_key_openssh
+      }
+    ]
+  })
+}
